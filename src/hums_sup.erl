1dd26f578dbf848074833fd34d29f2a391739541
Tristan Sloughter <t@crashfast.com>
2016-11-02 19:48:07 -0700

    add apache-2 license

--- src/hums_sup.erl
+++ src/hums_sup.erl
@@ -8,7 +8,8 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0,
+        stop/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -21,6 +22,10 @@
 
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+
+stop() ->
+    exit(whereis(?SERVER), normal).
 
 %%====================================================================
 %% Supervisor callbacks
